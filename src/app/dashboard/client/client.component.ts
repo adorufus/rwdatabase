5b1a999b101ae690b5576d4427f4420257b16cc8
Reingalrt <adorufus@gmail.com>
2024-05-02 17:59:00 +0700

    commit

--- src/app/dashboard/client/client.component.ts
+++ src/app/dashboard/client/client.component.ts
@@ -1,9 +1,15 @@
-import { Component, inject, OnInit } from '@angular/core'
+import { Component, inject, OnInit, ViewChild } from '@angular/core'
 import { AngularFirestore } from '@angular/fire/compat/firestore'
 import { Timestamp, where } from '@angular/fire/firestore'
 import { Observable } from 'rxjs'
 
-import { Dropdown, DropdownInterface, initDropdowns, initFlowbite } from 'flowbite'
+import {
+  Dropdown,
+  DropdownInterface,
+  initDropdowns,
+  initFlowbite,
+  Modal,
+} from 'flowbite'
 import { FilterSysService } from '../../services/filter-sys.service'
 
 declare var Datepicker: any
@@ -23,6 +29,8 @@ export class ClientComponent implements OnInit {
   type$: Observable<any[]> | undefined
   clientCompany$: Observable<any[]> | undefined
 
+  clientModal: Modal | undefined
+
   name: string = ''
   title: string = ''
   companyName: string = ''
@@ -37,8 +45,11 @@ export class ClientComponent implements OnInit {
   city: string = ''
   state: string = ''
   postalCode: string = ''
-  searchQuery: string = ''
+  clientUid: string | undefined
 
+  isUpdateData: boolean = false
+
+  searchQuery: string = ''
   companyNameSearch: string = ''
   addNewCompanyName: string = ''
 
@@ -49,7 +60,10 @@ export class ClientComponent implements OnInit {
 
   clients$: Observable<any[]> | undefined
 
-  constructor(private db: AngularFirestore, private filterSys: FilterSysService) {}
+  constructor(
+    private db: AngularFirestore,
+    private filterSys: FilterSysService,
+  ) {}
 
   // filter fields var
   selectedCompanyFilter: string = ''
@@ -59,8 +73,16 @@ export class ClientComponent implements OnInit {
   selectedEndDateFilter: Date | undefined
 
   ngOnInit(): void {
+    let targetElement = document.getElementById('default-modal')
+    let instanceOptions = {
+      id: 'default-modal',
+      override: true,
+    }
 
+    this.clientModal = new Modal(targetElement, undefined, instanceOptions)
     initFlowbite()
+
+    
     // this.company$ = collectionData(
     //   collection(this.firestore, 'companies'),
     // ) as Observable<Company[]>
@@ -73,7 +95,41 @@ export class ClientComponent implements OnInit {
       .collection('clients', (q) => q.orderBy('created_at', 'desc'))
       .valueChanges()
     this.type$ = this.db.collection('types').valueChanges()
-    
+  }
+
+  onCreateNewClick() {
+
+    this.clientModal?.show()
+  }
+
+  onEditClick(item: any) {
+    this.name = item.name ?? ''
+    this.title = item.title ?? ''
+    this.companyName = item.company_name ?? ''
+    this.phoneNumber = item.phone_number ?? 0
+    this.city = item.city ?? ''
+    this.emailAddress = item.email ?? ''
+    this.companyAddress = item.company_address ?? ''
+    this.streetAddress2 = item.second_address ?? ''
+    this.selectedCompany = item.company ?? ''
+    this.selectedType = item.type ?? ''
+    this.city = item.city ?? ''
+    this.state = item.state ?? ''
+    this.clientUid = item.id ?? ''
+
+    console.log('test')
+
+    this.clientModal?.show()
+
+    this.isUpdateData = true
+  }
+
+  onClose() {
+    if (this.isUpdateData) {
+      this.isUpdateData = false
+    }
+
+    this.clientModal?.hide()
   }
 
   onAddNewClientCompany() {
@@ -85,15 +141,25 @@ export class ClientComponent implements OnInit {
   }
 
   searchForCompany() {
-    this.clientCompany$ = this.db.collection('client_companies', ref => ref.where('name', '>=', this.companyNameSearch).where('name', '<=', this.companyNameSearch + '\uf8ff')).valueChanges()
+    this.clientCompany$ = this.db
+      .collection('client_companies', (ref) =>
+        ref
+          .where('name', '>=', this.companyNameSearch)
+          .where('name', '<=', this.companyNameSearch + '\uf8ff'),
+      )
+      .valueChanges()
   }
 
   saveClientCompany() {
-    let ref = this.db.collection('client_companies', ref => ref.where('name', '!=', this.companyName)).doc().ref
+    let ref = this.db
+      .collection('client_companies', (ref) =>
+        ref.where('name', '!=', this.companyName),
+      )
+      .doc().ref
 
-    if(this.companyName && this.companyName != ""){
+    if (this.companyName && this.companyName != '') {
       this.db.collection('client_companies').doc(ref.id).set({
-        name: this.companyName
+        name: this.companyName,
       })
     }
   }
@@ -103,7 +169,16 @@ export class ClientComponent implements OnInit {
     this.isFilterShown = false
     this.isFiltering = true
 
-    this.clients$ = this.filterSys.filterSearch({type: this.selectedTypeFilter, company: this.selectedCompanyFilter, industry: this.selectedIndustryFilter}, this.selectedStartDateFilter!, this.selectedEndDateFilter!, this.searchQuery)
+    this.clients$ = this.filterSys.filterSearch(
+      {
+        type: this.selectedTypeFilter,
+        company: this.selectedCompanyFilter,
+        industry: this.selectedIndustryFilter,
+      },
+      this.selectedStartDateFilter!,
+      this.selectedEndDateFilter!,
+      this.searchQuery,
+    )
   }
 
   onFilterClose() {
@@ -133,7 +208,7 @@ export class ClientComponent implements OnInit {
 
   onFilter() {
     console.log(`onFilterEntry & isFiltering: `, this.isFiltering)
-    if(this.isFiltering) {
+    if (this.isFiltering) {
       this.onClearFilter()
     } else {
       this.isFilterShown = !this.isFilterShown
@@ -147,7 +222,7 @@ export class ClientComponent implements OnInit {
         .ref.where('email', '==', this.emailAddress)
         .where('phone_number', '==', this.phoneNumber)
         .get()
-    ).empty
+    ).empty && !this.isUpdateData
   }
 
   onCompanySelect(e: Event) {
@@ -175,11 +250,11 @@ export class ClientComponent implements OnInit {
   }
 
   validateInput() {
-    if(!this.name || this.name == '') {
+    if (!this.name || this.name == '') {
       return false
     }
 
-    if(!this.phoneNumber || this.phoneNumber == 0) {
+    if (!this.phoneNumber || this.phoneNumber == 0) {
       return false
     }
 
@@ -187,40 +262,54 @@ export class ClientComponent implements OnInit {
   }
 
   async saveData() {
-
     let dbRef = this.db.collection('clients').doc().ref
     console.log(await this.checkIfValueExists())
 
-    if(this.validateInput()) {
+    if (this.validateInput()) {
       if (!(await this.checkIfValueExists())) {
-
         this.saveClientCompany()
 
+        let data: any = {
+          id: dbRef.id,
+          name: this.name,
+          title: this.title,
+          company_name: this.companyName,
+          phone_number: this.phoneNumber,
+          website_url: this.websiteUrl,
+          department: this.department,
+          email: this.emailAddress,
+          company_address: this.companyAddress,
+          second_address: this.streetAddress2,
+          company: this.selectedCompany,
+          city: this.city,
+          state: this.state,
+          postal_code: this.postalCode,
+          type: this.selectedType,
+          updated_at: Timestamp.fromDate(new Date(Date.now()))
+        }
+
+        if(!this.isUpdateData) {
+          data.created_at = Timestamp.fromDate(new Date(Date.now()))
+        }
+
         this.db
-          .collection('clients').doc(dbRef.id)
-          .set({
-            id: dbRef.id,
-            name: this.name,
-            title: this.title,
-            company_name: this.companyName,
-            phone_number: this.phoneNumber,
-            website_url: this.websiteUrl,
-            department: this.department,
-            email: this.emailAddress,
-            company_address: this.companyAddress,
-            second_address: this.streetAddress2,
-            company: this.selectedCompany,
-            city: this.city,
-            state: this.state,
-            postal_code: this.postalCode,
-            type: this.selectedType,
-            created_at: Timestamp.fromDate(new Date(Date.now())),
+          .collection('clients')
+          .doc(this.isUpdateData ? this.clientUid : dbRef.id)
+          .set(data, {
+            merge: true
           })
           .then((ref) => {
-            console.log(ref)
+            if (this.isUpdateData) {
+              console.log('update data completed')
+              this.isUpdateData = false
+            } else {
+              console.log(ref)
 
-            this.addNewCompanyName = ''
-            this.companyName = ''
+              this.addNewCompanyName = ''
+              this.companyName = ''
+            }
+
+            this.clientModal?.hide()
           })
           .catch((err) => {
             console.log(err)
